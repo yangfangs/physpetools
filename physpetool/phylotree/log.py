a2031e354cc3ce4481775a2ffbe60e96503bf307
Yang Fang <506528950@qq.com>
2016-05-18 21:37:57 +0800

    add the log system to physpe

---
+++ physpetool/phylotree/log.py
@@ -0,0 +1,25 @@
+import logging
+import os
+import argparse
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='logfile.log',
+                    filemode='w')
+
+console = logging.StreamHandler()
+
+console.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s %(name)s[line:%(lineno)d] %(levelname)s %(message)s')
+
+console.setFormatter(formatter)
+
+logging.getLogger('').addHandler(console)
+
+
+def getLogging(name):
+    return logging.getLogger(name)
+
+
