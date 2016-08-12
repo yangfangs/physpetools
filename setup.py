61095d36f8bfd6a350c5d61a1ca337d45456c6c1
Yang Fang <506528950@qq.com>
2016-08-12 19:38:55 +0800

    update setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ physpe_version =version
 try:
     LONG_DESCRIPTION = open("README.rst", "rb").read().decode("utf-8")
 except:
-    long_descr = "one command construct species phylogenetic tree "
+    long_descr = "One command auto reconstruct phylogenetic tree"
 setup(
     name="physpe",
     packages=find_packages(),
@@ -14,10 +14,10 @@ setup(
         "console_scripts": ['physpe = physpetool.physpe.physpe:main']
     },
     version=physpe_version,
-    description="Python command line to construct phylogenetic tree.",
+    description="One command line auto reconstruct phylogenetic tree.",
     long_description=LONG_DESCRIPTION,
     author="Yang Fang",
     author_email="yangfangscu@gmail.com",
     package_data={'': ['*.rst'], 'physpetool': ['database/*.db', 'softwares/*','database/*.txt']},
-    url="https://gitlab.com/xiaoxiaoyang/physpetool/tree/master",
+    url="https://github.com/xiaofeiyangyang/physpetools",
 )
