73c181beacf1bf803d577dfd323d9a8302720e23
Yang Fang <506528950@qq.com>
2016-08-14 15:43:26 +0800

    add annotation

--- physpetool/physpe/physpe.py
+++ physpetool/physpe/physpe.py
@@ -7,7 +7,7 @@ from physpetool.physpe import iview
 from physpetool.version import version_infor
 from physpetool.version import version
 
-physep_version =version
+physep_version = version
 """
 the main module as enter point and contain a main() function to invoke other
 script same as pipeline.
@@ -16,8 +16,7 @@ script same as pipeline.
 APP_DESC = ("""
         PHYSPE (%s):
 Reconstruct phylogenetic tree
-"""%(physep_version))
-
+""" % (physep_version))
 
 
 def main():
@@ -34,17 +33,17 @@ def start(input):
     parser = argparse.ArgumentParser(description=APP_DESC,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     subparser = parser.add_subparsers(title="RCONSTRUCT PHYLOGENETIC TREE")
-    #autobuild
+    # autobuild
     physpe_autobuid = subparser.add_parser("autobuild", help="Auto reconstruct phylogenetic tree")
     physpe_autobuid.set_defaults(func=autobuild.starting)
     autobuild.start_args(physpe_autobuid)
-    #combine
+    # combine
     physpe_combine = subparser.add_parser("combine", help="Combine phylogenetic tree")
     physpe_combine.set_defaults(func=combine.starting)
     combine.start_agrs(physpe_combine)
 
-    #iview
-    physpe_iview =subparser.add_parser("iview", help="View tree by iTol")
+    # iview
+    physpe_iview = subparser.add_parser("iview", help="View tree by iTol")
     physpe_iview.set_defaults(func=iview.starting)
     iview.start_agrs(physpe_iview)
 
@@ -54,14 +53,10 @@ def start(input):
         print ('Example: \nphyspe autobuild -h')
         return
     args = parser.parse_args(input[1:])
-    #call subparser
+    # call subparser
     args.func(args)
-
-
-
 
 
 if __name__ == '__main__':
     main()
     # start(['physpe','iview','-i','/home/yangfang/physpetools_data/TOF/testspenames.txt', '-r','phylum'])
-
