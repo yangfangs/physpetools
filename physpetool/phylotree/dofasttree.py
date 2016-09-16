64be3de64b4f7ad875e8d778905315f232a12f5e
Yang Fang <506528950@qq.com>
2016-09-16 16:20:49 +0800

    add FastTree

--- physpetool/phylotree/dofasttree.py
+++ physpetool/phylotree/dofasttree.py
@@ -33,24 +33,27 @@ logdofasttree = getLogging('FastTree')
 
 
 def doFastTree(inputfile, outputfile, FastTreepara, thread):
+    input_fasta = inputfile.replace('.phy', '')
     FastTreePath = getlocalpath()
     thread_to_str = str(thread)
     out_tree_name = os.path.join(outputfile, "FastTree.tree")
     if not os.path.exists(outputfile):
         os.mkdir(outputfile)
     if thread is '1':
-        cmd = FastTreePath + "/FastTree " + FastTreepara + inputfile + " >" + out_tree_name
+        cmd = FastTreePath + "/FastTree " + FastTreepara + input_fasta + " >" + out_tree_name
+        print (cmd)
         subprocess.call(cmd, shell=True)
     else:
         cmd_thread = "export OMP_NUM_THREADS=" + thread_to_str
         subprocess.call(cmd_thread, shell=True)
-        cmd = FastTreePath + "/FastTreeMP " + FastTreepara + inputfile + ">" + out_tree_name
+        cmd = FastTreePath + "/FastTreeMP " + FastTreepara + input_fasta + ">" + out_tree_name
+        print (cmd)
         subprocess.call(cmd, shell=True)
     logdofasttree.info("Phylogenetic species tree reconstruct by FastTree was completed")
 
 
 if __name__ == '__main__':
-    doFastTree("/home/yangfang/physpetools_data/test_fasttree/16srandata.fasta-gb1.phy",
+    doFastTree("/home/yangfang/physpetools/testdata/temp/concatenate20160916155942/concatenate.fasta-gb1.phy",
                "/home/yangfang/physpetools_data/test_fasttree/",
                "", "1"
                )
