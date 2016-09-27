0d99104a7199055d001d389b198aa16f2e61f439
Yang Fang <506528950@qq.com>
2016-09-27 16:27:01 +0800

    add coding

--- physpetool/version.py
+++ physpetool/version.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # ########################## Copyrights and License #############################
 #                                                                               #
 # Copyright 2016 Yang Fang <yangfangscu@gmail.com>                              #
