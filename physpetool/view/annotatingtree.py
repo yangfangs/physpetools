65cc814899993d6f15eda5eba667cbcff2c63098
Yang Fang <506528950@qq.com>
2017-08-15 09:34:52 +0800

    modify license

--- physpetool/view/annotatingtree.py
+++ physpetool/view/annotatingtree.py
@@ -2,21 +2,21 @@
 #                                                                              #
 # Copyright 2016 Yang Fang <yangfangscu@gmail.com>                             #
 #                                                                              #
-# This file is part of PhyspeTree.                                                 #
+# This file is part of PhyspeTree.                                             #
 # https://xiaofeiyangyang.github.io/physpetools/                               #
 #                                                                              #
-# PhySpeTree is free software: you can redistribute it and/or modify it under      #
+# PhySpeTree is free software: you can redistribute it and/or modify it under  #
 # the terms of the GNU Lesser General Public License as published by the Free  #
 # Software Foundation, either version 3 of the License, or (at your option)    #
 # any later version.                                                           #
 #                                                                              #
-# PhySpeTree is distributed in the hope that it will be useful, but WITHOUT ANY    #
+# PhySpeTree is distributed in the hope that it will be useful, but WITHOUT ANY#
 # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS    #
 # FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more #
 # details.                                                                     #
 #                                                                              #
 # You should have received a copy of the GNU Lesser General Public License     #
-# along with PhySpeTree. If not, see <http://www.gnu.org/licenses/>.               #
+# along with PhySpeTree. If not, see <http://www.gnu.org/licenses/>.           #
 #                                                                              #
 # ##############################################################################
 
